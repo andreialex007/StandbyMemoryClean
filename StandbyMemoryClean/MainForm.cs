59adb5f8c752267ff9e249bf58d9ffc44e48f10e
Andrei Ilushin <andreialexandrovich@gmail.com>
2019-11-16 00:27:00 +0300

    init repo

---
+++ StandbyMemoryClean/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StandbyMemoryClean
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            Form form = (Form)this;
+            form.ShowInTaskbar = false;
+            form.Opacity = 0;
+            InitializeComponent();
+            RunCleaner();
+        }
+
+        private async Task RunCleaner()
+        {
+            while (true)
+            {
+                CleanUtil.Program.ClearFileSystemCache(true);
+                await Task.Delay(10_000);
+            }
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            
+        }
+    }
+}
